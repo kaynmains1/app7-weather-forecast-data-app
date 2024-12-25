e99b9a84194580fc39fc9f61dd6e49dd5af1602a
Alex <alexeytimonov1@gmail.com>
2024-12-25 21:14:53 +0200

    final code v2

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ place = st.text_input(label="Place:", placeholder="Enter a city", key="place")
 days = st.slider(label="Forecast days", min_value=1, max_value=5, help="Select the number of forecast days", key="days")
 box = st.selectbox(label="Select data to view", options=("Temperature", "Sky"), key="box")
 st.subheader(f"{box} for the next {days} days in {place.capitalize()}")
+try:
+    if place:
+        filtered_data = get_data(place, days)
 
-if place:
-    filtered_data = get_data(place, days)
+        if box == "Temperature":
+            temperature = [dict["main"]["temp"] for dict in filtered_data]
+            temperature = [float(temp) / 10 for temp in temperature]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
+            chart = st.plotly_chart(figure)
 
-    if box == "Temperature":
-        temperature = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
-        chart = st.plotly_chart(figure)
-
-    if box == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow":"images/snow,png"}
-        sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
-        image = [images[condition] for condition in sky_condition]
-        st.image(image, width= 115)
+        if box == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow":"images/snow,png"}
+            sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+            image = [images[condition] for condition in sky_condition]
+            st.image(image, width= 115)
+except KeyError:
+    st.title("Please enter the correct name of a city")
